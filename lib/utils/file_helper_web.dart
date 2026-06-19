9bdcecef720cbc4ee8748e888e07bae6d34243a4
kdj <935637782@QQ.COM>
2026-06-19 14:45:48 +0800

    feat: 更新 CI/CD 配置以禁用图标树摇晃并增强文件下载功能

--- lib/utils/file_helper_web.dart
+++ lib/utils/file_helper_web.dart
@@ -1,6 +1,32 @@
-import 'dart:html' as html;
+import 'dart:js_interop';
 import 'dart:typed_data';
 import 'package:share_plus/share_plus.dart';
+
+/// JS Blob 构造函数映射
+extension type _JSBlob._(JSObject _) implements JSObject {
+  external _JSBlob(JSArray<JSUint8Array> parts);
+}
+
+/// JS URL 静态方法
+@JS('URL.createObjectURL')
+external JSString _createObjectURL(JSAny blob);
+
+@JS('URL.revokeObjectURL')
+external void _revokeObjectURL(JSString url);
+
+/// JS document 全局对象
+extension type _JSDocument(JSObject _) implements JSObject {
+  external JSObject createElement(JSString tagName);
+}
+
+/// JS HTML 元素
+extension type _JSHTMLElement(JSObject _) implements JSObject {
+  external void setAttribute(JSString qualifiedName, JSString value);
+  external void click();
+}
+
+@JS('document')
+external _JSDocument get _document;
 
 class FileHelper {
   static Future<String> tempDirPath() async {
@@ -33,11 +59,14 @@ class FileHelper {
 
   /// 触发浏览器下载文件
   static Future<void> triggerDownload(String name, Uint8List bytes) async {
-    final blob = html.Blob([bytes]);
-    final url = html.Url.createObjectUrlFromBlob(blob);
-    html.AnchorElement(href: url)
-      ..setAttribute('download', name)
-      ..click();
-    html.Url.revokeObjectUrl(url);
+    final jsArray = [bytes.toJS].toJS;
+    final blob = _JSBlob(jsArray);
+    final url = _createObjectURL(blob);
+    final anchorJS = _document.createElement('a'.toJS);
+    final anchor = _JSHTMLElement(anchorJS);
+    anchor.setAttribute('href'.toJS, url);
+    anchor.setAttribute('download'.toJS, name.toJS);
+    anchor.click();
+    _revokeObjectURL(url);
   }
 }
